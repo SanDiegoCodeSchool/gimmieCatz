453d253e07cdf3dcff2f3e222228e2998ce98e2c
Michael Roberts <michael@origincodeacademy.com>
2019-01-17 17:33:17 -0800

    adds some tests

--- cypress/integration/cats.spec.js
+++ cypress/integration/cats.spec.js
@@ -1,11 +1,27 @@
-describe('A simple logic test.', function() {
-    it('Does true equal true?', function() {
-      expect(false).to.equal(true)
-    })
-})
-
 describe('A simple functional test', function() {
     it('Visits the Gimmie Catz website', function() {
       cy.visit('/');
-    })
+    });
+    
+    it('Should allow typing in the comment box', function() {
+      cy.visit('/');
+      cy.get('h1').should('contain', 'RAWRR!!');
+    });
+
+    it('Should allow typing in the comment box', function() {
+      cy.visit('/');
+      cy.get('#comment-2').type('wow thats an awesome cat').should('have.value', 'wow thats an awesome cat');
+    });
+
+    it('Clicks on the button to add a comment', function() {
+      cy.visit('/');
+      cy.get('#comment-0').type('You just made my day!!🥰');
+      cy.get('#add-comment-0').click();
+
+    });
+
+    it('Clicks on the like button', function() {
+      cy.visit('/');
+      cy.get('#show-1').click();
+    });
 })
